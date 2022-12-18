1621915ef9946a69f602741b4af3c8e9a13a0b9c
ALsJourney <aldin.dino@outlook.com>
2022-12-18 18:16:00 +0100

    Created a small api, that returns a HashMap when a certain route is looked at

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use warp::Filter;
 
+use std::collections::HashMap;
+
+async fn hello(param: HashMap<String, String>) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("{:#?}", param))
+}
+
 #[tokio::main]
 async fn main() {
-    // Get /hello/warp => 200 Ok with body "Hello, warp!"
+    /*
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .map(|name| format!("Let's say hi to: {}!", name));
+    */
+
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::query::<HashMap<String, String>>())
+        .and(warp::path::end())
+        .and_then(hello);
 
     println!("Listening on http://127.0.0.1/3030");
-    
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
